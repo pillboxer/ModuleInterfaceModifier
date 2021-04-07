5082ee11343e4907ebfc7899f596ae3bc54bb2f2
Henry Cooper <henrycooper@outlook.com>
2021-04-07 16:15:06 +0100

    Initial Commit

---
+++ ModuleInterfaceModifier/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ModuleInterfaceModifier
+//
+//  Created by Henry Cooper on 07/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ModuleInterfaceModifier/ModuleInterfaceModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModuleInterfaceModifierApp.swift
+//  ModuleInterfaceModifier
+//
+//  Created by Henry Cooper on 07/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct ModuleInterfaceModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
